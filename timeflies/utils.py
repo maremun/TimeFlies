07ac5312c2b470cc648f95550d42cdb05638f3cf
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2016-12-18 01:40:51 +0300

    Remove echo functionality.

--- timeflies/utils.py
+++ timeflies/utils.py
@@ -4,7 +4,6 @@
 from pprint import pprint
 
 from .update_handlers import handle_callback_query, handle_commands
-from .telegram import send_message
 from .update_parser import detect_commands
 
 
@@ -20,20 +19,5 @@ def handle_update(update, session, database):
         handle_commands(commands, message, database)
     if callback_query:
         handle_callback_query(callback_query, database)
-
-    return update.get('update_id')
-    # return echo(update, session)
-
-
-def echo(update, sess):
-    m = update.get('message')
-
-    if m:
-        chat_id = m.get('chat').get('id')
-        username = m.get('from').get('username')
-        message_text = m.get('text')
-
-        text = '%s said %s' % (username, message_text)
-        send_message(chat_id, text, sess=sess)
 
     return update.get('update_id')
